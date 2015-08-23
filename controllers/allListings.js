1d51ff2aa68a23d78e596973e352dfd503013133
Kyle Boss <kyle@kyleboss.com>
2015-08-23 14:49:21 -0400

    Update listing after purchase

--- controllers/allListings.js
+++ controllers/allListings.js
@@ -13,13 +13,18 @@ var endDate = 1442980839;
 var findDriveways = function(db, callback) {
 	// db.collection('driveways').insert( { "merchant_id": "1", "address": "111 addr", "city": "Berkeley", "zipcode": "94709", "date_begin": 1440476620, "date_end": 1440476820 } )
    // var cursor = db.collection('driveways').find({ "zipcode": zipcode }, { "date_begin": { $lt : beginDate} }, { "date_end": {$gt: endDate } } );
-   var cursor = db.collection('driveways').find({});
+   var cursor = db.collection('driveways').find({"consumer_id":0});
    var htmlToReturn = "<div class='row items-container'>";
    cursor.each(function(err, doc) {
       assert.equal(err, null);
       if (doc != null) {
       	htmlToReturn += "<div class='item-container col-xs-3'><img class='item-img' src='" + doc.photo_url + "'><div class='row'><div class='col-xs-4'>" + doc.city + "</div><div class='col-xs-4'>" + doc.zipcode + "</div><div class='col-xs-4'>" + doc.price + "</div><div class='item-btn'> <button data-sc-key='sbpb_YzBlMTI5ZDItMTljZC00OWVkLTkyNGEtY2Y4Zjg3NjcxODAw' data-name='MasterDrive' data-description='Rent parking at " + doc.zipcode + "' data-reference = '" + doc._id + "' data-amount= '" + (doc.price * 100) + "' data-color='#12B830'> Reserve </button> </div></div></div>"
-      } else {
+	
+	db.collection('driveways').updateOne({"address":doc.address},{$set:{"consumer_id":1}});      
+
+
+
+} else {
         htmlToReturn += "</div>"
          callback(htmlToReturn);
       }
